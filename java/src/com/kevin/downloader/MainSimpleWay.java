688e48e6fda3f46aded8d3df58595736f8745b3c
shikaiwen <googlevsbing@126.com>
2015-07-28 15:48:34 +0800

    current version

--- java/src/com/kevin/downloader/MainSimpleWay.java
+++ java/src/com/kevin/downloader/MainSimpleWay.java
@@ -5,63 +5,17 @@ import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class MainSimpleWay {
 
-	static void initProxy(){
-		 System.setProperty("http.proxySet", "true");
-		 System.setProperty("http.proxyHost", "127.0.0.1");
-		 System.setProperty("http.proxyPort", "8888");
-	}
-	
-
-	
-	static void threadT(){
-		
-		ScheduledExecutorService exe =  Executors.newScheduledThreadPool(2);
-		exe.scheduleAtFixedRate(new Runnable(){
-			@Override
-			public void run() {
-				System.out.println(1);
-				try {
-					Thread.sleep(20000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}, 1, 2, TimeUnit.SECONDS);
-		
-		exe.scheduleAtFixedRate(new Runnable(){
-			@Override
-			public void run() {
-				System.out.println(2);
-			}
-		}, 1, 3, TimeUnit.SECONDS);
-	}
-	
-	
 	public static void main(String[] args) throws IOException {
-//		initProxy();
-		
-//		threadT();
 		new MainSimpleWay().go();
-//		fos.write(100);
-//		fos.flush();fos.close();
 	}
-	
 	
 	int threadCount = 5;
 	
-	
 	void go() throws IOException{
 		
-//		URL url = new URL("http://www.winpcap.org/install/bin/WinPcap_4_1_3.exe");
 		URL url = new URL("http://httpd.apache.org/images/httpd_logo_wide_new.png");
 		
 		String filename = url.toString().substring(url.toString().lastIndexOf("/"));
@@ -84,12 +38,8 @@ public class MainSimpleWay {
 			t.start();
 			
 		}
-		
-		
-		
 	}
 	
-
 	public int getThreadCount() {
 		return threadCount;
 	}
--- readMe.txt
+++ readMe.txt
@@ -1,3 +1,9 @@
-this is a multi-thread downloader , i plan to implement
-this in java , C , C++, maybe it's just for fun and for learning.
+this is a multi-thread downloader , i plan to implement,this in java , C , C++, 
+maybe it's just for fun and for learning.
 
+project progress 
+===============================
+basic version of java download has been implemented in MainSimpleWay.java
+and src/com/kevin/downloader/MainMultiThread.java.java
+
+MainSimpleWay is a simple version
